e2b3da9d26937ae516ed91bf34da556f5cedaa00
Eric Hale <eric_hale@comcast.net>
2020-05-06 13:14:35 -0600

    Add interation 1 and 2

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
+class School
+  attr_reader :start_time, :hours_in_school_day, :student_names
 
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
+  end
+
+  def add_student_name(student_name)
+    @student_names << student_name
+  end
+
+  def end_time
+    begining = start_time.to_i
+    total_time = begining + hours_in_school_day
+    total_time.to_s + ":00"
+  end
+end
